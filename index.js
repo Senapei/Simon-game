3f4ff9bc83387bb488da1add31a457ec0a7d7eed
Senapei <115939642+Senapei@users.noreply.github.com>
2023-03-09 15:17:19 +0000

    Add click button animation

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ let buttonColours = ["green", "red", "yellow", "blue"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+$("button").on("click", function (e) {
+  let userChosenColour = $(this).attr("class");
+
+  userChosenColour = userChosenColour.slice(0, userChosenColour.length - 7);
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function animatePress(currentColour) {
+  $("." + currentColour + "-button").addClass("pressed");
+  setTimeout(
+    () => $("." + currentColour + "-button").removeClass("pressed"),
+    100
+  );
+}
+
+function playSound(name) {
+  let buttonAudio = new Audio("./sounds/" + name + ".mp3");
+  buttonAudio.play();
+}
+
 function nextSequence() {
   let randomNum = Math.random() * 4;
   randomNum = Math.floor(randomNum);
@@ -13,13 +36,7 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  let randomColourAudio = new Audio("./sounds/" + randomChosenColour + ".mp3");
-  randomColourAudio.play();
-
-  $("button").on("click", function (e) {
-    let userChosenColour = e.currentTarget.className;
-
-    userChosenColour = userChosenColour.slice(0, userChosenColour.length - 7);
-    userClickedPattern.push(userChosenColour);
-  });
+  playSound(randomChosenColour);
 }
+
+//start game
