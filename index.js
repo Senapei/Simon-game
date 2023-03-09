92f1e017fab4256f103692c3d942dd2b693d983e
Senapei <115939642+Senapei@users.noreply.github.com>
2023-03-09 14:56:14 +0000

    Add clicking choices to an array

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function randomNumber() {
+let buttonColours = ["green", "red", "yellow", "blue"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+function nextSequence() {
   let randomNum = Math.random() * 4;
   randomNum = Math.floor(randomNum);
-  return randomNum;
+
+  let randomChosenColour = buttonColours[randomNum];
+  gamePattern.push(randomChosenColour);
+
+  $("." + randomChosenColour + "-button")
+    .fadeOut(100)
+    .fadeIn(100);
+
+  let randomColourAudio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+  randomColourAudio.play();
+
+  $("button").on("click", function (e) {
+    let userChosenColour = e.currentTarget.className;
+
+    userChosenColour = userChosenColour.slice(0, userChosenColour.length - 7);
+    userClickedPattern.push(userChosenColour);
+  });
 }
